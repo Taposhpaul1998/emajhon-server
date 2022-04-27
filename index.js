4379437527cd8ca2094a88edd8688362447731b4
Taposh paul <taposhp05@gmail.com>
2022-04-27 23:14:32 +0600

    loaded data to database

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express(); 
 //middleware
 app.use(cors());
-app.use(express());
+app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.imkfm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -17,18 +17,35 @@ async function run (){
         const productsCollection = client.db('emaJhon').collection('product');
 
         app.get('/product', async(req,res)=>{
+            console.log('query',req.query);
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
             const query = {};
             const cursor= productsCollection.find(query);
-            const products= await cursor.toArray();
+            let products;
+            if(page || size){
+                products = await cursor.skip(page*size).limit(size).toArray();
+            }
+            else{
+
+            }
             res.send(products);
         });
 
         app.get('/productCount', async(req,res)=>{
-            const query ={};
-            const cursor = productsCollection.find(query);
-            const count = await cursor.count();
+            const count = await productsCollection.estimatedDocumentCount();
             res.send({count})
         })
+
+        // use post to get products
+        app.post('/productsByKeys', async (req,res)=>{
+            const keys = req.body;
+            const ids = keys.map(id=> ObjectId(id))
+            const query = {_id:{$in:ids}}
+            const cursor = productsCollection.find(query)
+            const products = await cursor.toArray();
+            res.send(products);
+         })
     }
     finally{
 
